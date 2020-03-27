7cc267260a581263d13d999a47364ca55f46a8d1
Jonathon Chenvert <jchenvert1@gmail.com>
2020-03-26 21:11:15 -0700

    Initial Commit

---
+++ MotorNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotorNotes
+//
+//  Created by Jonathon Chenvert on 3/26/20.
+//  Copyright © 2020 Jonathon Chenvert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
