bd73201de6574efc1013ba57fad01f653848392c
Jonathon Chenvert <jchenvert1@gmail.com>
2020-04-11 20:27:49 -0700

    #iss1

---
+++ MotorNotes/Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  MotorNotes
+//
+//  Created by Jonathon Chenvert on 4/11/20.
+//  Copyright © 2020 Jonathon Chenvert. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+    }
+}
