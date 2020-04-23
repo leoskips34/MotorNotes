b2a10945b5e48b3bca72d5d3204ba251e49294d3
Jonathon Chenvert <jchenvert1@gmail.com>
2020-04-22 18:43:17 -0700

    Added TableView to HomeVC and retrieval from db into TableView

--- MotorNotes/View Controllers/HomeViewController.swift
+++ MotorNotes/View Controllers/HomeViewController.swift
@@ -9,13 +9,15 @@
 import UIKit
 import Firebase
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var leadingCon: NSLayoutConstraint!
     @IBOutlet weak var trailingCon: NSLayoutConstraint!
+    @IBOutlet weak var tableView: UITableView!
     
     var menuOut = false
     var db: Firestore!
+    var carArray = [[String: String]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,17 +25,53 @@ class HomeViewController: UIViewController {
         // Firestore setup
         db = Firestore.firestore()
         
+        // TableView stuff
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         loadData()
     }
     
     // MARK: - Firestore data loading
     func loadData() {
+        
         db.collection("users").document(Constants.Authentication.user).collection("cars").getDocuments() { (querySnapshot, err) in
             if let err = err {
+                
                 print("Error getting documents: \(err)")
             } else {
+                
                 for document in querySnapshot!.documents {
-                    print(document.data())
+                    
+                    let data = document.data()
+                    let carNickname = data["carnickname"] as? String ?? ""
+                    let carMake = data["make"] as? String ?? ""
+                    let carModel = data["model"] as? String ?? ""
+                    let carYear = data["year"] as? String ?? ""
+                    let carOdometer = data["odometer"] as? String ?? ""
+                    let carLicensePlateNumber = data["licenseplatenumber"] as? String ?? ""
+                    let carVin = data["vinnumber"] as? String ?? ""
+                    let carRegistrationDate = data["registrationdate"] as? String ?? ""
+                    let carColor = data["carcolor"] as? String ?? ""
+                    
+                    let newCar = [
+                        "carnickname": carNickname,
+                        "make": carMake,
+                        "model": carModel,
+                        "year": carYear,
+                        "odometer": carOdometer,
+                        "licenseplatenumber": carLicensePlateNumber,
+                        "vinnumber": carVin,
+                        "registrationdate": carRegistrationDate,
+                        "carcolor": carColor,
+                    ]
+                    
+                    self.carArray.append(newCar)
+                    print(self.carArray)
+                }
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
                 }
             }
         }
@@ -58,5 +96,32 @@ class HomeViewController: UIViewController {
             print("The hamburger menu button has been pressed")
         }
 
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return carArray.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "CarCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? CarCell else {
+            fatalError("The dequeued cell is not an instance of CarCell")
+        }
+        
+        let car = carArray[indexPath.row]
+        
+        cell.carNicknameLabel.text = car["carnickname"]
+        cell.carOdometerLabel.text = car["odometer"]
+        cell.carRegistrationDateLabel.text = car["registrationdate"]
+        
+        print("Cell passed")
+        return cell
+        
     }
 }
